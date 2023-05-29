1b98f614d8b90114b0a3d2931d499bf7fef871ae
Araa Rashek <araa.rashek@gmail.com>
2023-05-30 00:29:58 +0300

    Adding Loading Skeleton

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,14 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width="400px" borderRadius={15} overflow="hidden" padding={10}>
+      <Skeleton height="200px" />
+      <CardBody>
+        <SkeletonText></SkeletonText>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
