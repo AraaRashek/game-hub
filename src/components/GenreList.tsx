5f8f79316b38ca4fd3d25643d87a8db97f86f87a
Araa Rashek <araa.rashek@gmail.com>
2023-06-02 22:56:49 +0300

    fixes && show a spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,18 +1,35 @@
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useData from "../hooks/useData";
+import getCroppedImageUrl from "../hooks/image-crop";
 
 interface Genre {
   id: number;
   name: string;
+  image_background: string;
 }
 
 const GenreList = () => {
-  const { data } = useData<Genre>("/genres");
+  const { data, error, isLoading } = useData<Genre>("/genres");
+
+  if (isLoading) return <Spinner />;
+  if (error) return null;
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={2}>
+          <HStack>
+            <Image
+              boxSize="64px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              {genre.name}
+            </Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
