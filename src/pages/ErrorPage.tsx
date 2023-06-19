179b710912f5acb688be1071e94ab8b71b2bd853
Araa Rashek <araa.rashek@gmail.com>
2023-06-19 20:46:50 +0300

    handle Errors

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,21 @@
+import { Heading, Text } from "@chakra-ui/react";
+import React from "react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <NavBar />
+      <Heading textAlign="center">Error</Heading>
+      <Text textAlign="center" mt={4}>
+        {isRouteErrorResponse(error)
+          ? "This Page does not exsit"
+          : "an error has accured"}
+      </Text>
+    </>
+  );
+};
+
+export default ErrorPage;
